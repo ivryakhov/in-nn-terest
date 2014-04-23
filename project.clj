c9d0764e7113c7d51096aef96d8080f3c8a0763f
ivryakhl <ivryakhov@gmail.com>
2014-04-23 19:56:52 +0000

    starting a templating with enfocus

--- project.clj
+++ project.clj
@@ -3,13 +3,20 @@
   :url "https://github.com/ivryakhov/in-nn-terest"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2202"]
+                 [enlive "1.1.5"]
+                 [ring "1.2.2"]
+                 [compojure "1.1.6"]]
 
   :source-paths ["src/clj" "src/cljs"]
-  :plugins [[lein-cljsbuild "1.3.0"]]
+  :plugins [[lein-cljsbuild "1.0.3"]
+            [lein-ring "0.8.10"]]
 
+  :ring {:handler in-nn-terest.core/handler}
+  
   :cljsbuild {:builds
               [{:source-paths ["src/cljs"]
-                :compiler {:output-to "resources/public/js/in-nn-terest.js"
+                :compiler {:output-to "resources/public/js/main.js"
                            :optimizations :whitespace
                            :pretty-print true}}]})
--- src/clj/in_nn_terest/core.clj
+++ src/clj/in_nn_terest/core.clj
@@ -1,6 +1,13 @@
-(ns in-nn-terest.core)
+(ns in-nn-terest.core
+  (:use compojure.core)
+  (:require [compojure.handler :as handler]
+            [compojure.route :as route]
+            [ring.util.response :refer (resource-response)]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defroutes app-routes
+  (GET "/" [] (resource-response "blank.html" {:root "publlic"}))
+  (route/resources "/")
+  (route/not-found "Page not found"))
+
+(def handler
+  (handler/site app-routes))
