ef96b1847f429c1a181febab37dc01137b3c898d
ivryakhl <ivryakhov@gmail.com>
2014-05-15 20:19:30 +0000

    Doing datomic

--- src/clj/in_nn_terest/base.clj
+++ src/clj/in_nn_terest/base.clj
@@ -1,4 +1,34 @@
 (ns in-nn-terest.base
-  (require [datomic.api :as d]))
+  (:require [datomic.api :as d]))
 
-(def db-url "datomic:free://localhost:4334/in-nn-terest")
+(def uri "datomic:free://localhost:4334/in-nn-terest")
+(def *conn* nil)
+
+(def schema [{:db/ident       :place/name
+             :db/valueType   :db.type/string
+             :db/cardinality :db.cardinality/one
+             :db/id          (d/tempid :db.part/db)
+             :db.install/_attribute :db.part/db}
+            
+            {:db/ident       :place/lat
+             :db/valueType   :db.type/double
+             :db/cardinality :db.cardinality/one
+             :db/id          (d/tempid :db.part/db)
+             :db.install/_attribute :db.part/db}
+
+            {:db/ident       :place/lng
+             :db/valueType   :db.type/double
+             :db/cardinality :db.cardinality/one
+             :db/id          (d/tempid :db.part/db)
+             :db.install/_attribute :db.part/db}
+
+            {:db/ident       :place/events
+             :db/valueType   :db.type/ref
+             :db/cardinality :db.cardinality/many
+             :db/id          (d/tempid :db.part/db)
+             :db.install/_attribute :db.part/db}])
+
+(defn init-db []
+  (d/create-database uri)
+  (set! *conn* (d/connect uri))
+  (d/transact *conn* schema))
