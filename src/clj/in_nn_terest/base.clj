b9b71d3e90052aceff3e6a2b1c2bf0c0751425af
ivryakhl <ivryakhov@gmail.com>
2014-05-17 19:09:29 +0000

    Datomic schema

--- src/clj/in_nn_terest/base.clj
+++ src/clj/in_nn_terest/base.clj
@@ -4,29 +4,7 @@
 (def uri "datomic:free://localhost:4334/in-nn-terest")
 (def *conn* nil)
 
-(def schema [{:db/ident       :place/name
-             :db/valueType   :db.type/string
-             :db/cardinality :db.cardinality/one
-             :db/id          (d/tempid :db.part/db)
-             :db.install/_attribute :db.part/db}
-            
-            {:db/ident       :place/lat
-             :db/valueType   :db.type/double
-             :db/cardinality :db.cardinality/one
-             :db/id          (d/tempid :db.part/db)
-             :db.install/_attribute :db.part/db}
-
-            {:db/ident       :place/lng
-             :db/valueType   :db.type/double
-             :db/cardinality :db.cardinality/one
-             :db/id          (d/tempid :db.part/db)
-             :db.install/_attribute :db.part/db}
-
-            {:db/ident       :place/events
-             :db/valueType   :db.type/ref
-             :db/cardinality :db.cardinality/many
-             :db/id          (d/tempid :db.part/db)
-             :db.install/_attribute :db.part/db}])
+(def schema (load-file "resources/datomic/schema.edn"))
 
 (defn init-db []
   (d/create-database uri)
