8b23635081d1692f0d1588df18f27852695a52df
ivryakhl <ivryakhov@gmail.com>
2014-05-19 19:55:25 +0000

    Sample data

--- src/clj/in_nn_terest/base.clj
+++ src/clj/in_nn_terest/base.clj
@@ -1,12 +1,12 @@
 (ns in-nn-terest.base
   (:require [datomic.api :as d]))
 
-(def uri "datomic:free://localhost:4334/in-nn-terest")
-(def *conn* nil)
+;;(def uri "datomic:free://localhost:4334/in-nn-terest-db")
+(def uri "datomic:mem://localhost:4334/in-nn-terest-db")
+(def conn (d/connect uri))
 
 (def schema (load-file "resources/datomic/schema.edn"))
 
 (defn init-db []
   (d/create-database uri)
-  (set! *conn* (d/connect uri))
-  (d/transact *conn* schema))
+  @(d/transact conn schema))
